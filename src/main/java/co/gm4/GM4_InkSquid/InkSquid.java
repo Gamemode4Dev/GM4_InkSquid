e45c4c8c64ee74172863b13fb3efb287443caf2b
minerguy31 <minerguy31@gmail.com>
2016-07-05 13:06:27 -0600

    Add configuration options

--- src/main/java/co/gm4/GM4_InkSquid/InkSquid.java
+++ src/main/java/co/gm4/GM4_InkSquid/InkSquid.java
@@ -10,22 +10,27 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class InkSquid extends JavaPlugin {
+	public static int RADIUS, POLL_FREQ, BLIND_DUR;
 	@Override
 	public void onEnable(){
+		saveDefaultConfig();
+		RADIUS = getConfig().getInt("squid-radius");
+		POLL_FREQ = getConfig().getInt("poll-freq");
+		BLIND_DUR = getConfig().getInt("blind-duration");
 		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 			public void run() {
 				for(Player p : Bukkit.getOnlinePlayers()) {
 					Material m = p.getLocation().getBlock().getType();
 					if(m == Material.WATER || m == Material.STATIONARY_WATER) {
-						for(Entity e : p.getNearbyEntities(5, 5, 5)) {
+						for(Entity e : p.getNearbyEntities(RADIUS, RADIUS, RADIUS)) {
 							if(e.getType() == EntityType.SQUID) {
-								p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,100,5,false,false));
+								p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS,BLIND_DUR,5,false,false));
 								break;
 							}
 						}
 					}
 				}
 			}
-		}, 0, 80);
+		}, 0, POLL_FREQ);
 	}
 }
